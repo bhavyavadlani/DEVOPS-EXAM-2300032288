ce0a8c2d8df830c83207d04cf6b548ca91124bed
vadlanibhavya <vadlani.bhavya@gmail.com>
2025-10-24 16:30:51 +0530

    updated

--- FRONTEND/src/VoterApp.jsx
+++ FRONTEND/src/VoterApp.jsx
@@ -19,7 +19,7 @@ import {
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function VoterApp() {
-  const baseUrl = `http://localhost:2000`;
+  const baseUrl = `http://localhost:2027/2300032288-backend`;
   const [voter, setVoter] = useState({
     id: "",
     name: "",
